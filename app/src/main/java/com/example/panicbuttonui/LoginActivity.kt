c2818d016672a440eee96fe5e63f11367dacabdf
imambudi808 <imambudi808@gmail.com>
2021-02-05 21:10:14 +0800

    Test projek UI

---
+++ app/src/main/java/com/example/panicbuttonui/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.example.panicbuttonui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private val image="https://pesonasia.com/public/uploads/posts/1612527868selamat datang.PNG"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setStatusBackgroundColor(getMyColor(R.color.white))
+        setLightStatusBar(true)
+        imgIllustration.loadImage(image)
+        btnLogin.setOnClickListener {
+            login()
+        }
+
+    }
+
+    private fun login(){
+        val phone=edtPhone.text.toString()
+        val password=edtPassword.text.toString()
+        if (isNullOrEmpty(arrayOf(phone, password))) {
+            toast("Phone dan password tidak boleh kosong")
+            return
+        }else{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+}
